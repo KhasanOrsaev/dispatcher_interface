fa6aaeb1f527026050b144ac483c8e04e08779b5
Хасан Орсаев <k.orsaev@fin.media>
2019-10-21 11:35:24 +0300

    edit

--- dispatcher-interface.go
+++ dispatcher-interface.go
@@ -5,14 +5,26 @@ type Source interface {
 	ReadData(inChannel chan <- map[interface{}][]byte) error
 	// Confirm подтверждение о записи в назначение
 	Confirm(confirmChannel <- chan interface{}) error
+	// SetConfig инициализировать конфигурацию
+	SetConfig(f []byte) error
+	OpenConnection()error
+	CloseConnection()error
 }
 
 type Destination interface {
 	// WriteData запись данных в назначение
 	WriteData(outChannel <- chan map[interface{}][]byte, confirmChannel chan <- interface{}, crashChannel chan <- []byte) error
+	// SetConfig инициализировать конфигурацию
+	SetConfig(f []byte) error
+	OpenConnection()error
+	CloseConnection()error
 }
 
 type Crash interface {
 	// SaveData сохранение записи при которой произошла ошибка
 	SaveData(crashChannel <- chan []byte)
+	// SetConfig инициализировать конфигурацию
+	SetConfig(f []byte) error
+	OpenConnection()error
+	CloseConnection()error
 }
